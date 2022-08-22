3464f55a2d4fa563ca2075b6da7966eec200ceb2
Deva1422 <61109323+Deva1422@users.noreply.github.com>
2022-08-22 19:21:10 +0530

    conflict

--- src/main/java/gitrepo/GitDemo1.java
+++ src/main/java/gitrepo/GitDemo1.java
@@ -5,7 +5,7 @@ public class GitDemo1 {
 	public static void main(String[] args) {
 		System.out.println("Hi Git");
 		
-		System.out.println("2nd commit from gitt");
+		System.out.println("2nd commit from gitt con");
 	}
 
 }
